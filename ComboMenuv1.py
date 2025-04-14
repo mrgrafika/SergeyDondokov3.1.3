ff86cf4cdd5b6bc0ba470e8aa7185e3316bd1a9d
Sergey Dondokov <sergey.dondokov@gmail.com>
2025-04-14 12:09:25 -0700

    Update ComboMenuv1.py

--- ComboMenuv1.py
+++ ComboMenuv1.py
@@ -1,3 +1,12 @@
 '''Iteration 1
  Ask the user for a type of sandwich (chicken $5.25, beef $6.25, tofu $5.75).
  Have the program output the user’s sandwich selection to verify that the program is working correctly.'''
+
+print("Chicken is 1, beef is 2, tofu is 3.")
+selection = input("What type of sandwich do you want? Chicken ($5.25), beef ($6.25), or tofu ($5.75)? ")
+if selection == "1":
+    print("You have selected a chicken sandwich.")
+elif selection == "2":
+    print("You have selected a beef sandwich.")
+elif selection == "3":
+    print("You have selected a tofu sandwich.")
